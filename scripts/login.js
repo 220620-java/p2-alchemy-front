57ab52706a62d074eb3f4c339ca93dd0453bd2fe
Evgeniy Ko <evgeniyko@Evgeniys-MBP.attlocal.net>
2022-08-07 14:39:57 -0400

    register

--- scripts/login.js
+++ scripts/login.js
@@ -38,3 +38,27 @@ async function login() {
         msgSpan.innerText = 'Incorrect credentials. Please try again.';
     }
 }
+
+async function register() {
+    let registerSpan = document.getElementById('registerSpan');
+    //registerSpan.innerText='';
+
+    let newUsername = document.getElementById('registerUsername').value;
+    let newPassword = document.getElementById('registerPassword').value;
+    let firstName = document.getElementById('registerFirstName').value;
+    let lastName = document.getElementById('registerLastName').value;
+
+    let userRegistration = {username:newUsername, password:newPassword, firstname:firstName, lastname:lastName};
+    let registrationResp = await fetch(apiUrl+'/users', {
+        method:'POST',
+        body:JSON.stringify(userRegistration),
+        headers:new Headers({
+            'Content-Type':'application/json'
+        })
+    });
+    if (resp.status===201) {
+        registerSpan.innerText='Account Registered';
+    } else {
+        registerSpan.innerText='Username already exists';
+    }
+}
